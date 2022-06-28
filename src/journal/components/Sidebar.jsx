6c66146bb894c8bd962d8b70dfda1e61931766c6
Anthony Acosta M <anthony082008@hotmail.es>
2022-06-27 22:44:39 -0500

    App frontend finished (styles)

---
+++ src/journal/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { TurnedInNot } from '@mui/icons-material';
+
+export const Sidebar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Anthony Acosta
+          </Typography>
+        </Toolbar>
+        <Divider />
+
+        <List>
+          {['Enero', 'Febrero', 'Marzo', 'Abril'].map((month) => (
+            <ListItem key={month} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+
+                <Grid container>
+                  <ListItemText primary={month} />
+                  <ListItemText
+                    secondary={'Test de prueba inserting line just for testing purposes.'}
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,28 @@
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>;
-    </>
+    <JournalLayout>
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+    </JournalLayout>
   );
 };
